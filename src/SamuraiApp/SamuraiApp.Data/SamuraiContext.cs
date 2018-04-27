a2ab112e4288d39dfceb2dab68664d6b68340f42
Muhammad Ihsan <mihsan191@gmail.com>
2018-04-28 09:18:29 +1000

    Add many to many relationship between Samurai and Battle

--- src/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ src/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -8,10 +8,18 @@ namespace SamuraiApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Battle> Battles { get; set; }
         public DbSet<Quote> Quotes { get; set; }
+        public DbSet<SamuraiBattle> SamuraiBattles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = SamuraiData; Trusted_Connection = True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>()
+                .HasKey(s => new { s.BattleId, s.SamuraiId });
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- src/SamuraiApp/SamuraiApp.Domain/Battle.cs
+++ src/SamuraiApp/SamuraiApp.Domain/Battle.cs
@@ -7,13 +7,14 @@ namespace SamuraiApp.Domain
     {
         public Battle()
         {
-            Samurais = new List<Samurai>();
+            SamuraiBattles = new List<SamuraiBattle>();
         }
 
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public List<Samurai> Samurais { get; set; }
+        //public List<Samurai> Samurais { get; set; }
+        public List<SamuraiBattle> SamuraiBattles { get; set; }
     }
 }
