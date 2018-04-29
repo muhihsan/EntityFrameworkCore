ee96e9acddd66c7295e9056cb40e916287aa0269
Muhammad Ihsan <mihsan191@gmail.com>
2018-04-29 16:30:56 +1000

    Add SomeUI project with InsertSamurai method and MyLoggerProvider to log to console

---
+++ src/SamuraiApp/SomeUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
+
+namespace SomeUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InsertSamurai();
+        }
+
+        private static void InsertSamurai()
+        {
+            var samurai = new Samurai { Name = "Ihsan" };
+            using (var context = new SamuraiContext())
+            {
+                context.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+            }
+        }
+    }
+}
